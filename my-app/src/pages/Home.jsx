73c992102b807db20bfeb576f333c1d8c6122097
capprimmir <capprimmir21@gmail.com>
2018-03-24 17:31:00 -0400

    incorporate a button that creates a transition CSS effent in the Home page

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -3,13 +3,31 @@ import Navbar from '../components/navbar/Navbar.jsx';
 import Footer from '../components/footer/Footer.jsx';
 import Jumbotron from '../components/jumbotron/Jumbotron.jsx';
 
+const styles ={
+    transition: 'all 1s ease-out'
+};
+
 class Home extends Component {
+    constructor() {
+        super();
+        this.state={
+            opacity: 1,
+        };
+        
+        this.onHide = this.onHide.bind(this)
+    }
+    onHide(){
+        this.setState({
+            opacity: 0,
+        });
+    }
     render(){
         return (
-            <div>
+            <div className="home">
                 <Navbar />
                 <Jumbotron title="Welome" subtitle="To My First React Website!" />
-                <div className="container">
+                <div className="container"
+                style={{...styles, opacity: this.state.opacity, transform: 'scale(' + this.state.scale +')'}}>
                     <h2>Welcome!!</h2>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley 
                     of type and scrambled it to make a type specimen book.
@@ -17,7 +35,11 @@ class Home extends Component {
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley 
                     of type and scrambled it to make a type specimen book.
                     </p>
+
+                    <button type="button" onClick={this.onHide}class="btn btn-info">Hide Content</button>
+            
                 </div>
+                
                 <Footer />
 
             </div>
